8376030a885062cc61c428637c8e833ef730c648
Mira Yang <myang@ucvts.org>
2017-01-25 22:08:15 +0000

    Edited the final project!

--- foodapi.py
+++ foodapi.py
@@ -10,8 +10,9 @@ endpoint = "http://food2fork.com/api/search"
 api_key =  "7d04269097d461c362524a4d13987a64"
 #allow the user to choose a specific food to make
 foodchoice = input("Choose a food that you would like to make!")
+recipecount = input("Choose how many recipe results you would like!")
 #sort by rating, set up url 
-payload = {"key": api_key, "count": 15, "recipes": "title", "q":foodchoice}
+payload = {"key": api_key, "count": recipecount, "recipes": "title", "q":foodchoice}
 
 #search for the recipe in the database
 req = requests.get(endpoint, params=payload)
@@ -19,6 +20,8 @@ req = requests.get(endpoint, params=payload)
 results = req.json()
 #create a Data Frame, which will be the "recipes" outputs
 rec = results["recipes"]
+#for n in results['recipes']:
+    #print(n['social_rank'])
 
 #use vplot to organize multiple graphs 
 from bokeh.io import output_file, save, vplot
@@ -30,7 +33,7 @@ p = Bar(df, "publisher", color = "navy", fill_alpha = 0.75, bar_width=1.1, title
 #create a histogram by importing it from bokeh first
 from bokeh.charts import Histogram
 #create the chart; however, since most of the recipes are ranked very close to 100, the graph should look like a single line at/near 100.
-p1 = Histogram(df, "social_rank", color = "lavender", fill_alpha = 1, bar_width=1.9, title="Social Rank of Recipe by Interval")
+p1 = Histogram(df, "social_rank", color = "lavender", fill_alpha = 1, bar_width=1.9, bins=200, title="Social Rank of Recipe by Interval")
 #save the graphs for preview at foodapi.html 
 v = vplot(p, p1) 
 save(v)
